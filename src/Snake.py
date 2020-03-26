394cdf06063e29257777b73254a98f486e211500
Sergey Gorovov <gor-serg@bk.ru>
2020-03-26 11:08:44 +0300

    added looping around the screen and continuous motion of the snake

--- src/Snake.py
+++ src/Snake.py
@@ -11,14 +11,25 @@ class Snake(object):
         self.color = color
         self.rows = self.window_size//self.cell_size
         self.x = 5
+        self.dx = 1
         self.y = 5
+        self.dy = 0
         self.body.append(Cube(self.x, self.y, window_size, cell_size, color))
 
     def move(self, direct):
-        if direct is None:
-            return
-        self.x += direct[0]
-        self.y += direct[1]
+        if direct is not None:
+            self.dx = direct[0]
+            self.dy = direct[1]
+        self.x += self.dx
+        if self.x >= self.window_size//self.cell_size:
+            self.x = 0
+        elif self.x < 0:
+            self.x = self.window_size//self.cell_size - 1
+        self.y += self.dy
+        if self.y > self.window_size//self.cell_size - 1:
+            self.y = 0
+        elif self.y < 0:
+            self.y = self.window_size // self.cell_size - 1
         self.body.append(Cube(self.x, self.y, self.window_size, self.cell_size, self.color))
         if len(self.body) > 15:
             self.body.pop(0)
